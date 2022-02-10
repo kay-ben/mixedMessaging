0bbf67af81fa61c7b979c39a4b17d816adce6cbb
kbenoit <77555044+kay-ben@users.noreply.github.com>
2022-02-10 15:07:08 -0600

    Combine data elements into a single object.

--- horoscope.js
+++ horoscope.js
@@ -1,14 +1,12 @@
 // Define Data Elements
-const zodiac = ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"];
-const eventList = ["romantic relationships","career choices","friendships","sexual partners","big purchases"];
-const negative = ["cautious","diligent","prudent","alert","wary"];
-const positive = ["enthusiasm","zeal","passion","haste","tenacity"];
-
+const horoscopeInfo = {
+    const zodiac = ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"];
+    const eventList = ["romantic relationships","career choices","friendships","sexual partners","big purchases"];
+    const negative = ["cautious","diligent","prudent","alert","wary"];
+    const positive = ["enthusiasm","zeal","passion","haste","tenacity"];
+}
 
 const event1 = eventList[Math.floor(Math.random()*5)];
 const event2 = eventList.filter(x => x !== event1)[Math.floor(Math.random()*5)];
-
-
-
 
 console.log(`Your sun sign is ${zodiac[Math.floor(Math.random()*12)]}. Be ${negative[Math.floor(Math.random()*5)]} with any ${event1} in the near future. Pursue new ${event2} with ${positive[Math.floor(Math.random()*5)]}.`) 
