409282776e11cf5ac40171d166e8a19dbdba472b
kbenoit <77555044+kay-ben@users.noreply.github.com>
2022-02-11 16:19:31 -0600

    Create and run daily horoscope function.

--- horoscope.js
+++ horoscope.js
@@ -1,15 +1,20 @@
 // Define Data Elements
 const horoscope = {
     zodiac: ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"],
-    eventList: ["romantic relationships","career choices","friendships","sexual partners","big purchases"],
-    outlook: ["cautious","diligent","prudent","alert","wary","enthusiasm","zeal","passion","haste","tenacity"],
-}
+    eventList: ["romantic relationships","career choices","friendships","sexual partners","financial decisions"],
+    outlook: ["cautious","diligent","prudent","alert","wary","enthusiastic","zealous","passionate","quick","tenacious"],
+};
 
 //Random Number Generator
 const randomNumberGenerator = (num) => {
     return Math.floor(Math.random()*num)
-}
+};
 
-//Your sun sign is
-//Be ${negative[Math.floor(Math.random()*5)]} with any ${event1} in the near future.
-//Pursue new ${event2} with ${positive[Math.floor(Math.random()*5)]}.
+//Daily Horoscope Function
+const dailyHoroscope = () => {
+    console.log(`Your sun sign is ${horoscope.zodiac[randomNumberGenerator(horoscope.zodiac.length)]}.`);
+    console.log(`Be ${horoscope.outlook[randomNumberGenerator(horoscope.outlook.length)]} with new ${horoscope.eventList[randomNumberGenerator(horoscope.eventList.length)]} in the near future.`);
+};
+
+//Generate Daily Horoscope
+dailyHoroscope();
