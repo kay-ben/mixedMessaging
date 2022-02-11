90b3081f5b816ec6ad4f80817e5f2f38937a5f84
kbenoit <77555044+kay-ben@users.noreply.github.com>
2022-02-10 19:00:36 -0600

    Add unique events to horoscope object.

--- horoscope.js
+++ horoscope.js
@@ -1,14 +1,22 @@
 // Define Data Elements
-const zodiac = ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"];
-const eventList = ["romantic relationships","career choices","friendships","sexual partners","big purchases"];
-const negative = ["cautious","diligent","prudent","alert","wary"];
-const positive = ["enthusiasm","zeal","passion","haste","tenacity"];
+const horoscope = {
+    zodiac: ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"],
+    eventList: ["romantic relationships","career choices","friendships","sexual partners","big purchases"],
+    negative: ["cautious","diligent","prudent","alert","wary"],
+    positive: ["enthusiasm","zeal","passion","haste","tenacity"],
+    //Choose 2 unique events from event list
+    event1: this.eventList[Math.floor(Math.random()*horoscope.eventList.length)],
+    event2: this.eventList.filter(x => !== this.event1)[Math.floor(Math.random()*(horoscope.eventList.length-1))],
+}
 
+//Random Number Generator
+const randomNumberGenerator = (num) => {
+    return Math.floor(Math.random()*num)
+}
 
-const event1 = eventList[Math.floor(Math.random()*5)];
-const event2 = eventList.filter(x => x !== event1)[Math.floor(Math.random()*5)];
+console.log(event1);
+console.log(event2);
 
-
-
-
-console.log(`Your sun sign is ${zodiac[Math.floor(Math.random()*12)]}. Be ${negative[Math.floor(Math.random()*5)]} with any ${event1} in the near future. Pursue new ${event2} with ${positive[Math.floor(Math.random()*5)]}.`) 
+//Your sun sign is
+//Be ${negative[Math.floor(Math.random()*5)]} with any ${event1} in the near future.
+//Pursue new ${event2} with ${positive[Math.floor(Math.random()*5)]}.
