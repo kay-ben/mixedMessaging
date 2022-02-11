33c97d5fcab7f210231a00a4a4a8dc7ee6f18278
kbenoit <77555044+kay-ben@users.noreply.github.com>
2022-02-11 16:01:21 -0600

    Reduce number of events. Combine negative and positives into outlook.

--- horoscope.js
+++ horoscope.js
@@ -2,20 +2,13 @@
 const horoscope = {
     zodiac: ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"],
     eventList: ["romantic relationships","career choices","friendships","sexual partners","big purchases"],
-    negative: ["cautious","diligent","prudent","alert","wary"],
-    positive: ["enthusiasm","zeal","passion","haste","tenacity"],
-    //Choose 2 unique events from event list
-    event1: this.eventList[Math.floor(Math.random()*horoscope.eventList.length)],
-    event2: this.eventList.filter(x => !== this.event1)[Math.floor(Math.random()*(horoscope.eventList.length-1))],
+    outlook: ["cautious","diligent","prudent","alert","wary","enthusiasm","zeal","passion","haste","tenacity"],
 }
 
 //Random Number Generator
 const randomNumberGenerator = (num) => {
     return Math.floor(Math.random()*num)
 }
-
-console.log(event1);
-console.log(event2);
 
 //Your sun sign is
 //Be ${negative[Math.floor(Math.random()*5)]} with any ${event1} in the near future.
